1dd63bf2985d523ffcf03d219818bf5f43213412
shivam <shivam@SHIVAM-PC>
2014-08-27 07:08:12 +0530

    updated doc. for class AdjacencyList

--- Symbol Table/src/com/symboltable/AdjacencyList.java
+++ Symbol Table/src/com/symboltable/AdjacencyList.java
@@ -2,46 +2,64 @@ package com.symboltable;
 
 /**
  * An adjacency list of strings, implemented with a linked list.
+ * 
  * @author shivam
  *
  */
 public class AdjacencyList {
-	
+
 	/**
 	 * Beginning element of the list (first node).
 	 */
-	private Node firstElement; 
-	
+	private Node firstElement;
+
 	/**
 	 * Size of the list.
 	 */
-	private int size; 
-	
+	private int size;
+
 	/**
-	 * Helper linked list data type to build nodes 
+	 * Helper linked list data type to build nodes
+	 * 
 	 * @author shivam
 	 *
 	 */
 	private static class Node {
+
+		/**
+		 * Name of the node.
+		 */
 		String name;
+
+		/**
+		 * Next node.
+		 */
 		Node next;
 
+		/**
+		 * Constructor to set name of the node and next node.
+		 * 
+		 * @param name
+		 * @param next
+		 */
 		Node(String name, Node next) {
 			this.name = name;
 			this.next = next;
 		}
 	}
-	
+
 	/**
 	 * Method to check whether list is empty.
+	 * 
 	 * @return True, if empty.
 	 */
 	public boolean isEmpty() {
 		return (firstElement == null);
 	}
-	
+
 	/**
 	 * Method to return size of list.
+	 * 
 	 * @return Size of list.
 	 */
 	public int size() {
